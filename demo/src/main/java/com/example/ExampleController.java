e0869b8c315139b2e7ebbcd3a49bfd6db4e91056
shingo_ito <itoshingo@se-project.co.jp>
2017-04-03 21:54:31 +0900

    springbootのサンプルをコミット

---
+++ demo/src/main/java/com/example/ExampleController.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 
+ * springBootサンプルControllerクラス
+ * @author shingo_ito
+ *
+ */
+@Controller
+@EnableAutoConfiguration
+public class ExampleController {
+
+	/**
+	 * 「Hello World!!」を表示するだけ
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping("/")
+	public String index(Model model) {
+		model.addAttribute("value", "Hello World!!");
+		return "index";
+	}
+}
